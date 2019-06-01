5da00b61c937b80442c5d73c6ca7af8c561c935a
dxjester <patbenitez@gmail.com>
2019-06-01 12:35:40 -0400

    Update crime_analysis.py

--- crime_analysis.py
+++ crime_analysis.py
@@ -16,5 +16,23 @@ import csv
 #-------------------- PHASE 1: Import & Clean the Dataset --------------------#
 #-----------------------------------------------------------------------------#
 
-crime_data = pd.read_csv('/Users/patrickbenitez/Desktop/Georgia Tech/Codebook/Python Projects/US Crime Project/US Crime Analysis/uscrime.txt')
-crime_data.head(10)
+
+infile = r'/Users/patrickbenitez/Desktop/Georgia Tech/Codebook/Python Projects/US Crime Project/US Crime Analysis/uscrime.txt'
+outfile = r'/Users/patrickbenitez/Desktop/Georgia Tech/Codebook/Python Projects/US Crime Project/US Crime Analysis/uscrime.csv'
+
+import_df = pd.read_csv(infile, sep='\t', lineterminator='\r')
+import_df.head(10)
+
+# validate data types for all attributes
+import_df.dtypes
+
+# create a copy of the clean dataframe import
+master_df = import_df
+
+# convert columns to a list 
+column_list = list(master_df)
+column_list
+
+# create an empty dictionary to store five number summaries for each column
+five_num_dict = {}
+five_num_dict.fromkeys(["Column Header", "Min", "25% Quartile", "Mean", "75% Quartile", "Max"])
