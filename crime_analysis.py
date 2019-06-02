b938a223669dcb5029be4615cdf468e4a141df05
dxjester <patbenitez@gmail.com>
2019-06-02 19:35:10 -0400

    Update crime_analysis.py

--- crime_analysis.py
+++ crime_analysis.py
@@ -33,6 +33,10 @@ master_df = import_df
 column_list = list(master_df)
 column_list
 
-# create an empty dictionary to store five number summaries for each column
-five_num_dict = {}
-five_num_dict.fromkeys(["Column Header", "Min", "25% Quartile", "Mean", "75% Quartile", "Max"])
+# create a list of dictionaries storing five number summaries for each of the attributes
+five_num_sum = []
+for col in column_list:
+    temp = {'Column Name': col, 'Min': 'Null', '25% Quartile': 'Null', 'Mean': 'Null', '75% Quartile': 'Null', 'Max': 'Null' }
+    five_num_sum.append(temp)
+
+five_num_sum
